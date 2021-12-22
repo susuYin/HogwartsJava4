29696e56963bdb6dee48177dfd234583ea4d0841
Alicegao <315442614@qq.com>
2021-12-22 16:05:39 +0800

    编写测试方法，获取yaml步骤，执行

--- src/test/java/com/ceshiren/hogwarts/web/BasePage.java
+++ src/test/java/com/ceshiren/hogwarts/web/BasePage.java
@@ -2,6 +2,7 @@ package com.ceshiren.hogwarts.web;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasePage {
 
